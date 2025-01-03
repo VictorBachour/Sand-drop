a052d01f7a9365379a283b259cb8f24022e9aacb
vbacho <victorbachour@gmail.com>
2025-01-02 18:50:31 -0800

    sand falls normally

--- sanddrop.py
+++ sanddrop.py
@@ -7,24 +7,42 @@ class SandDrop:
         self.sand_pixel_size = 10
         self.canvas = tk.Canvas(self.root, bg="black", height=400, width=400)
         self.canvas.pack()
+        self.canvas.update()
+        self.rows = self.canvas.winfo_height() // self.sand_pixel_size
+        self.cols = self.canvas.winfo_width() // self.sand_pixel_size
+        self.current_grid = np.zeros((self.rows, self.cols), dtype=int)
         self.game_running()
 
+
     def game_running(self):
-        self.root.update()
-        cols = self.canvas.winfo_width() // self.sand_pixel_size
-        rows = self.canvas.winfo_height() // self.sand_pixel_size
+        self.draw_board()
+        self.update_board()
+        self.root.after(100, self.game_running)
+        self.root.mainloop()
 
-        current_grid = np.zeros((rows, cols), dtype=int)
-
-        for i in range(rows):
-            for j in range(cols):
+    def draw_board(self):
+        self.canvas.delete("all")
+        for i in range(self.rows):
+            for j in range(self.cols):
                 x1 = i * self.sand_pixel_size
                 y1 = j * self.sand_pixel_size
                 x2 = x1 + self.sand_pixel_size
                 y2 = y1 + self.sand_pixel_size
-                if(np.zeros[i][j] == 1):
+                if(self.current_grid[i][j] == 1):
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="black", width=2)
                 else:
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="white", width=2)
-        self.root.mainloop()
+
+    def update_board(self):
+        new_grid = np.copy(self.current_grid)
+        for i in range(self.cols):
+            for j in range(self.rows):
+                if j < self.rows:
+                        if(self.current_grid[i][j] == 1):
+                            if j < self.rows - 1:
+                                below = self.current_grid[i][j + 1]
+                                if below == 0:
+                                    new_grid[i][j + 1] = 1
+                                    new_grid[i][j] = 0
+        self.current_grid = new_grid
 SandDrop()
